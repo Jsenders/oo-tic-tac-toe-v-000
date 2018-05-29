075cf8bcce1327b453dea8b08eb2e0c4675690d1
Joseph Senders <jasenders@gmail.com>
2018-05-29 15:59:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,10 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
     @board[index] = current_player
   end
 
+  def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
   def valid_move?(index)
   index.between?(0,8) && !position_taken?(board, index)
   end
